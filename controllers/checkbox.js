72b5024335a29d15c320ac8664d56b043bcb316a
lishengzxc <eric@lishengcn.cn>
2017-04-15 10:22:22 +0800

    (UPDATE) add checkbox

---
+++ controllers/checkbox.js
@@ -0,0 +1,25 @@
+const checkbox = async function (ctx, next) {
+  let {
+    size = 15,
+    checked = false,
+    color = '01D48F'
+  } = ctx.query;
+
+  size = parseInt(size, 10);
+  checked = !!(+checked);
+
+  let points = [
+    [0.13333333333333333, 0.5],
+    [0.43333333333333335, 0.7333333333333333],
+    [0.8333333333333334, 0.23333333333333334]
+  ].map(value => value.map(item => item * size)).join(' ');
+
+  await ctx.render('checkbox', {
+    size,
+    points,
+    color,
+    checked
+  });
+};
+
+module.exports = checkbox;
--- routers.js
+++ routers.js
@@ -1,10 +1,13 @@
 const Router = require('koa-router');
 
 const progress = require('./controllers/progress');
+const checkbox = require('./controllers/checkbox');
+
 const svgContentType = require('./middlewares/svgContentType');
 
 const router = new Router();
 
 router.get('/progress', svgContentType, progress);
+router.get('/checkbox', svgContentType, checkbox);
 
 module.exports = router;
